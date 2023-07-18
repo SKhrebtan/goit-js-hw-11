f2933953d9bb217487e07173b0bbde52b5062fa1
SKhrebtan <shrebtan@gmail.com>
2023-07-18 18:27:03 +0300

    fix lightbox

--- src/index.js
+++ src/index.js
@@ -3,17 +3,30 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 axios.defaults.headers.common["x-api-key"] = "38328283-3432d4ee282ba2126186b7660";
-const API_KEY = '38328283-3432d4ee282ba2126186b7660';
 
+const API_KEY = '38328283-3432d4ee282ba2126186b7660';
 const searchImgForm = document.querySelector('#search-form');
 const galleryContainer = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
 // axios.get(BASE_URL).then(console.log)
 searchImgForm.addEventListener('submit', onFormSearch);
+loadMoreBtn.addEventListener('click', onLoadMorePhotos);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+let searchValue = '';
+let currentPage = 1;
 
 function onFormSearch(e) {
     e.preventDefault();
-    const searchRequest = e.currentTarget.elements.searchQuery.value;
-    const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}&q=${searchRequest}&image_type=photo&image_type=photo&orientation=horizontal&safesearch=true`;
+  searchValue = e.currentTarget.elements.searchQuery.value;
+  galleryContainer.innerHTML = '';
+  currentPage = 1;
+    const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}&q=${searchValue}&image_type=photo&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=40`;
     fetch(BASE_URL)
     .then(response => response.json())
         .then(response => {
@@ -37,14 +50,40 @@ function onFormSearch(e) {
 </div>`
             }
             ).join('');
-            galleryContainer.innerHTML = markup;
+          galleryContainer.insertAdjacentHTML('beforeend', markup);
+           lightbox.refresh();
+        })
+ 
+};
 
-
-    })
+function onLoadMorePhotos() {
+  currentPage += 1;
+    const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}&q=${searchValue}&image_type=photo&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=40`;
+    fetch(BASE_URL)
+    .then(response => response.json())
+        .then(response => {
+            const markup = response.hits.map((item) => {
+         return `<div class="photo-card">
+         <a class="gallery-link" href="${item.largeImageURL}"><img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes${item.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views${item.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments${item.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads${item.downloads}</b>
+    </p>
+  </div>
+</div>`
+            }
+            ).join('');
+          galleryContainer.insertAdjacentHTML('beforeend', markup);
+           lightbox.refresh();
+        })
 }
 
-let lightbox = new SimpleLightbox('.photo-card a', {
-  captions: true,
-  captionsData: 'alt',
-  captionDelay: 250,
-});
