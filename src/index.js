6f27ba471626ffbf7672d30fabd9d7aea15bf367
SKhrebtan <shrebtan@gmail.com>
2023-07-19 22:33:05 +0300

    fix second bootstrap spinner

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ const searchImgForm = document.querySelector('#search-form');
 const galleryContainer = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 const spinner = document.querySelector('.spin');
-
+const btnSpinner = document.querySelector('.spinner-border');
+const loadSpan = document.querySelector('.load-span');
 let searchValue = '';
 let currentPage = 1;
 let currentTotalHits = 0;
@@ -45,20 +46,21 @@ loadMoreBtn.addEventListener('click', onLoadMorePhotos);
 async function onFormSearch(e) {
   e.preventDefault();
   searchValue = e.currentTarget.elements.searchQuery.value;
-  galleryContainer.innerHTML = '';
   spinner.style.display = 'none';
+  btnSpinner.classList.add('is-hidden');
   loadMoreBtn.style.display = 'none';
   currentPage = 1;
   if (searchValue === '') {
+    Notiflix.Notify.info('Буляска уведіть запит');
     return;
   }
- 
   try {
     const response = await fetchImages(searchValue, currentPage);
     if (response.totalHits === 0) {
       Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
       return;
     }
+    galleryContainer.innerHTML = '';
     currentTotalHits = response.hits.length;
     Notiflix.Notify.success(`Hooray! We found ${response.totalHits} images.`);
     if (currentTotalHits < 40) {
@@ -78,7 +80,8 @@ async function onLoadMorePhotos() {
   currentPage += 1;
   loadMoreBtn.disabled = true;
   spinner.style.display = 'block';
-  loadMoreBtn.textContent = 'Loading...';
+  btnSpinner.classList.remove('is-hidden');
+  loadSpan.textContent = 'Loading...';
   try {
     const response = await fetchImages(searchValue, currentPage);
     if (response.totalHits === 0) {
@@ -87,8 +90,9 @@ async function onLoadMorePhotos() {
     }
     currentTotalHits += response.hits.length;
     loadMoreBtn.disabled = false;
-     spinner.style.display = 'none';
-    loadMoreBtn.textContent = 'Load more';
+    spinner.style.display = 'none';
+    btnSpinner.classList.add('is-hidden');
+    loadSpan.textContent = 'Load more';
     if (currentTotalHits === response.totalHits) {
        Notiflix.Notify.info('We are sorry, but you have reached the end of search results');
       loadMoreBtn.style.display = 'none';
