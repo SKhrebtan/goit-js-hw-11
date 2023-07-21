42ca3d7b934498ddb848eea2a52086f69ffefd74
SKhrebtan <131458411+SKhrebtan@users.noreply.github.com>
2023-07-21 07:29:47 +0000

    Deploying to gh-pages from @ SKhrebtan/goit-js-hw-11@3acb869285fd2892755ddfb0ebbfb294fc2fc542 🚀

--- index.192240bc.js
+++
@@ -1,2 +0,0 @@
-const o=document.getElementById("myBtn");o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})})),window.onscroll=function(){document.body.scrollTop>400||document.documentElement.scrollTop>400?o.style.display="block":o.style.display="none"};
-//# sourceMappingURL=index.192240bc.js.map
--- index.4c5df7fa.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.getElementById("myBtn");o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})})),window.onscroll=function(){document.body.scrollTop>400||document.documentElement.scrollTop>400?o.style.display="block":o.style.display="none"}}();
-//# sourceMappingURL=index.4c5df7fa.js.map
